9fc02225b7f32e98153624a6689be06c9464daee
Tobias Haeussler <mail@tobiashaeussler.com>
2020-05-03 12:11:55 +0200

    Eval in global space.

--- lib/shader/dspbr.glsl
+++ lib/shader/dspbr.glsl
@@ -13,6 +13,8 @@
  * limitations under the License.
  */
 
+struct Geometry { vec3 n, t, b; };
+
 vec3 fresnel_schlick(vec3 f0, vec3 f90, float theta)
 {
     return f0 + (f90-f0)*pow(abs(1.0-theta), 5.0);
@@ -36,60 +38,60 @@ float sqr(float x)
 // http://jcgt.org/published/0003/02/03/paper.pdf
 
 // eq. 80
-float projected_roughness(in vec2 alpha, in vec3 w)
+float projected_roughness(vec2 alpha, vec3 w, Geometry g)
 {
-    float sin_theta_2 = 1.0 - w.z * w.z;
+    float sin_theta_2 = 1.0 - sqr(dot(w, g.n));
     float inv_sin_theta_2 = 1.0 / sin_theta_2;
-    float cos_phi_2 = sqr(w.x) * inv_sin_theta_2;
-    float sin_phi_2 = sqr(w.y) * inv_sin_theta_2;
+    float cos_phi_2 = sqr(dot(w, g.t)) * inv_sin_theta_2;
+    float sin_phi_2 = sqr(dot(w, g.b)) * inv_sin_theta_2;
 
     return sqrt(cos_phi_2 * sqr(alpha.x) + sin_phi_2 * sqr(alpha.y));    
 }
 
 // eq. 86
-float ggx_smith_lambda(in vec2 alpha, in vec3 w)
+float ggx_smith_lambda(vec2 alpha, vec3 w, Geometry g)
 {
-    float sin_theta_2 = 1.0 - w.z * w.z;
+    float sin_theta_2 = 1.0 - sqr(dot(w, g.n));
 
     if (sin_theta_2 < 0.0001) {
         return 0.0;
     }
 
-    float alpha_w = projected_roughness(alpha, w);
+    float alpha_w = projected_roughness(alpha, w, g);
 
-    float tan_theta = sqrt(sin_theta_2) / abs(w.z);
+    float tan_theta = sqrt(sin_theta_2) / abs(dot(w, g.n));
     float a = 1.0 / (alpha_w * tan_theta);
 
     return 0.5 * (-1.0 + sqrt(1.0 + 1.0 / sqr(a)));
 }
 
 // eq. 43
-float ggx_smith_g1(in vec2 alpha, in vec3 w, in vec3 wh)
+float ggx_smith_g1(vec2 alpha, vec3 w, vec3 wh, Geometry g)
 {
     if (dot(w, wh) < 0.0) {
         return 0.0;
     }
 
-    return 1.0 / (1.0 + ggx_smith_lambda(alpha, w));
+    return 1.0 / (1.0 + ggx_smith_lambda(alpha, w, g));
 }
 
 // eq. 99
-float ggx_smith_g2(in vec2 alpha, in vec3 wi, in vec3 wo, in vec3 wh)
+float ggx_smith_g2(vec2 alpha, vec3 wi, vec3 wo, vec3 wh, Geometry g)
 {
     if (dot(wo, wh) * dot(wi, wh) < 0.0) {
         return 0.0;
     }
 
-    float lambda_wi = ggx_smith_lambda(alpha, wi);
-    float lambda_wo = ggx_smith_lambda(alpha, wo);
+    float lambda_wi = ggx_smith_lambda(alpha, wi, g);
+    float lambda_wo = ggx_smith_lambda(alpha, wo, g);
 
     return 1.0 / (1.0 + lambda_wi + lambda_wo);
 }
 
 // GGX distribution, eq. 85
-float ggx_eval(vec2 alpha, vec3 wh)
+float ggx_eval(vec2 alpha, vec3 wh, Geometry g)
 {
-    if (wh.z < 0.0) {
+    if (dot(wh, g.n) < 0.0) {
         return 0.0;
     }
 
@@ -100,28 +102,28 @@ float ggx_eval(vec2 alpha, vec3 wh)
     // float norm = 1.0 / (PI * alpha.x * alpha.y);
     // return norm * (1.0 / sqr(hx + hy + hn));
 
-    float cos_theta_2 = sqr(wh.z);
+    float cos_theta_2 = sqr(dot(wh, g.n));
     float cos_theta_4 = sqr(cos_theta_2);
     float sin_theta_2 = 1.0 - cos_theta_2;
-    float tan_theta_2 = sqr(sqrt(sin_theta_2) / wh.z);
+    float tan_theta_2 = sqr(sqrt(sin_theta_2) / dot(wh, g.n));
 
     if (sin_theta_2 < 0.0001) {
         return 0.0;
     }
 
     float inv_sin_theta_2 = 1.0 / sin_theta_2;
-    float cos_phi_2 = sqr(wh.x) * inv_sin_theta_2;
-    float sin_phi_2 = sqr(wh.y) * inv_sin_theta_2;
+    float cos_phi_2 = sqr(dot(wh, g.t)) * inv_sin_theta_2;
+    float sin_phi_2 = sqr(dot(wh, g.b)) * inv_sin_theta_2;
 
     return 1.0 / (PI * alpha.x * alpha.y * cos_theta_4 * sqr(1.0 + tan_theta_2 * (cos_phi_2 / sqr(alpha.x) + sin_phi_2 / sqr(alpha.y))));
 }
 
 // GGX distribution of visible normals, eq. 16
-float ggx_eval_vndf(vec2 alpha, vec3 wi, vec3 wh)
+float ggx_eval_vndf(vec2 alpha, vec3 wi, vec3 wh, Geometry g)
 {
-    float d = ggx_eval(alpha, wh);
-    float g1 = ggx_smith_g1(alpha, wi, wh);
-    return g1 * abs(dot(wi, wh)) * d / abs(wi.z);
+    float d = ggx_eval(alpha, wh, g);
+    float g1 = ggx_smith_g1(alpha, wi, wh, g);
+    return g1 * abs(dot(wi, wh)) * d / abs(dot(wi, g.n));
 }
 
 // Sample GGX distribution of visible normals
@@ -173,11 +175,11 @@ vec3 average_fresnel(vec3 f0, vec3 f90)
     return 20./21. * f0 + 1./21. * f90;
 }
 
-vec3 microfacet_ggx_smith_eval_ms(vec3 f0, vec3 f90, vec2 alpha_anisotropic, float NdotV, float NdotL)
+vec3 microfacet_ggx_smith_eval_ms(vec3 f0, vec3 f90, vec2 alpha_uv, vec3 wi, vec3 wo, Geometry g)
 {
-    float alpha = alpha_anisotropic.x;//sqrt(alpha_anisotropic.x*alpha_anisotropic.y);
-    float Ewi = directional_albedo_ggx(alpha, NdotV);
-    float Ewo = directional_albedo_ggx(alpha, NdotL);
+    float alpha = sqrt(alpha_uv.x*alpha_uv.y);
+    float Ewi = directional_albedo_ggx(alpha, abs(dot(wi, g.n)));
+    float Ewo = directional_albedo_ggx(alpha, abs(dot(wo, g.n)));
     float Eavg = average_albedo_ggx(alpha);
     float ms = (1.0 - Ewo) * (1.0 - Ewi) / (PI * (1.0 - Eavg));
     vec3 Favg = average_fresnel(f0, f90);
@@ -185,17 +187,19 @@ vec3 microfacet_ggx_smith_eval_ms(vec3 f0, vec3 f90, vec2 alpha_anisotropic, flo
     return ms * f;
 }
 
-vec3 microfacet_ggx_smith_eval(vec3 f0, vec3 f90, vec2 alpha, vec3 wi, vec3 wo, vec3 wh)
+vec3 microfacet_ggx_smith_eval(vec3 f0, vec3 f90, vec2 alpha, vec3 wi, vec3 wo, vec3 wh, Geometry geo)
 {
-    if (abs(wi.z) < 0.0001 || abs(wo.z) < 0.0001) {
+    if (abs(dot(wi, geo.n)) < 0.0001 || abs(dot(wo, geo.n)) < 0.0001) {
         return vec3(0.0);
     }
 
+    alpha = clamp(alpha, vec2(0.001), vec2(1.0));
+
     vec3 f = fresnel_schlick(f0, f90, dot(wi, wh));
     
-    float d = ggx_eval(alpha, wh);
-    float g = ggx_smith_g2(alpha, wi, wo, wh);
-    return (f * g * d) / abs(4.0 * wi.z * wo.z);
+    float d = ggx_eval(alpha, wh, geo);
+    float g = ggx_smith_g2(alpha, wi, wo, wh, geo);
+    return (f * g * d) / abs(4.0 * dot(wi, geo.n) * dot(wo, geo.n));
 }
 
 vec3 sample_hemisphere_cos(vec2 uv, out float pdf) {
@@ -212,7 +216,12 @@ vec3 microfacet_ggx_smith_sample(in vec2 alpha, in vec3 wi, in vec2 uv, out floa
     vec3 wo = reflect(-wi, wh);
 
     float dwh_dwo = 1.0 / (4.0 * abs(dot(wo, wh)));
-    pdf = ggx_eval_vndf(alpha, wi, wh) * dwh_dwo;
+
+    Geometry g;
+    g.n = vec3(0, 0, 1);
+    g.t = vec3(1, 0, 0);
+    g.b = vec3(0, 1, 0);
+    pdf = ggx_eval_vndf(alpha, wi, wh, g) * dwh_dwo;
 
     return wo;
 }
@@ -235,15 +244,15 @@ float coupled_diffuse(vec2 alpha, float dot_wi_n, float dot_wo_n, float e0)
     return (1.0 - Ewo) * (1.0 - Ewi) / (PI * (1.0 - Eavg));
 }
 
-vec3 diffuse_bsdf_eval(const in MaterialClosure c, float dot_wi_n, float dot_wo_n) {
+vec3 diffuse_bsdf_eval(const in MaterialClosure c, vec3 wi, vec3 wo, Geometry g) {
     float lambert = ONE_OVER_PI;
-    float coupled = coupled_diffuse(c.alpha, dot_wi_n, dot_wo_n, max_(c.f0*c.specular_tint));
+    float coupled = coupled_diffuse(c.alpha, abs(dot(wi, g.n)), abs(dot(wo, g.n)), max_(c.f0*c.specular_tint));
     vec3 diffuse_color = c.albedo * (1.0-c.metallic);
     return diffuse_color * mix(lambert, coupled, c.specular);
 }
 
-float diffuse_bsdf_pdf(in vec3 wi, in vec3 wo) {
-    return same_hemisphere(wi, wo) ? abs(wo.z)*ONE_OVER_PI : 0.0;
+float diffuse_bsdf_pdf(vec3 wi, vec3 wo, Geometry g) {
+    return abs(dot(wo, g.n)) * ONE_OVER_PI;
 }
 
 vec3 diffuse_bsdf_importance(vec3 tint) {
@@ -274,53 +283,49 @@ float directional_albedo_sheen(float cos_theta, float alpha)
 }
 
 vec3 sheen_layer(vec3 base, float sheen_intensity, vec3 sheen_color, float sheen_roughness,
-    float dot_wi_n, float dot_wo_n, float dot_wh_n)
+    vec3 wi, vec3 wo, vec3 wh, Geometry g)
 {
     float alpha = max(sheen_roughness, 0.07);
-    float invAlpha = 1.0 / alpha;
-    float cosThetaHSq = dot_wh_n*dot_wh_n;
-    float sinThetaHSq = max(1.0 - cosThetaHSq, 0.001);
-    float D = (2.0 + invAlpha) * pow(abs(sinThetaHSq), 0.5 * invAlpha) / (2.0 * PI);
-    float G = 1.0 / (1.0 + lambda_sheen(dot_wi_n, alpha) + lambda_sheen(dot_wo_n, alpha));
-    float sheen = G * D / (4.0 * dot_wi_n * dot_wo_n);
+    float inv_alpha = 1.0 / alpha;
+    float cos_theta_i = dot(wi, g.n);
+    float cos_theta_o = dot(wo, g.n);
+    float cos_theta_h_2 = sqr(dot(wh, g.n));
+    float sin_theta_h_2 = max(1.0 - cos_theta_h_2, 0.001);
+    float D = (2.0 + inv_alpha) * pow(abs(sin_theta_h_2), 0.5 * inv_alpha) / (2.0 * PI);
+    float G = 1.0 / (1.0 + lambda_sheen(cos_theta_i, alpha) + lambda_sheen(cos_theta_o, alpha));
+    float sheen = G * D / (4.0 * cos_theta_i * cos_theta_o);
 
-    float Ewi = sheen_intensity * max_(sheen_color) * directional_albedo_sheen(dot_wi_n, alpha);
-    float Ewo = sheen_intensity * max_(sheen_color) * directional_albedo_sheen(dot_wo_n, alpha);
+    float Ewi = sheen_intensity * max_(sheen_color) * directional_albedo_sheen(cos_theta_i, alpha);
+    float Ewo = sheen_intensity * max_(sheen_color) * directional_albedo_sheen(cos_theta_o, alpha);
 
     return base * min(1.0 - Ewi, 1.0 - Ewo) + sheen_intensity * sheen_color * sheen;
-    //return base + sheen_intensity * sheen_color * sheen;
 }
 
-
 vec3 coating_layer(vec3 base, float clearcoat, float clearcoat_roughness,
-    vec3 wi, vec3 wo, vec3 wh)
+    vec3 wi, vec3 wo, vec3 wh, Geometry g)
 {
     vec2 alpha_coating = vec2(clearcoat_roughness*clearcoat_roughness);
-    vec3 coating = microfacet_ggx_smith_eval(vec3(0.04), vec3(1.0), alpha_coating, wi, wo, wh);
-    vec3 Fcv = clearcoat * fresnel_schlick(vec3(0.04), vec3(1.0), wi.z);
-    vec3 Fcl = clearcoat * fresnel_schlick(vec3(0.04), vec3(1.0), wo.z);
-    return base * (1.0 - max_(max(Fcv, Fcl))) + clearcoat * coating;
-    //return base + clearcoat * coating;
+    vec3 coating = microfacet_ggx_smith_eval(vec3(0.04), vec3(1.0), alpha_coating, wi, wo, wh, g);
+    vec3 Fcv = clearcoat * fresnel_schlick(vec3(0.04), vec3(1.0), abs(dot(wi, g.n)));
+    vec3 Fcl = clearcoat * fresnel_schlick(vec3(0.04), vec3(1.0), abs(dot(wo, g.n)));
+    return base * (1.0 - max_(max(Fcv, Fcl))); + clearcoat * coating;
 }
 
 vec3 dspbr_eval(const in MaterialClosure c, vec3 wi, vec3 wo) {
     vec3 wh = normalize(wi + wo);
-    float dot_wh_n = dot(wh, c.n);
 
-    vec3 wi_ = c.to_local * wi;
-    vec3 wo_ = c.to_local * wo;
-    vec3 wh_ = c.to_local * wh;
+    Geometry g;
+    g.n = c.n;
+    g.t = c.t;
+    g.b = cross(c.n, c.t);
 
-    float dot_wi_n = wi_.z;
-    float dot_wo_n = wo_.z;
-
-    vec3 diffuse = diffuse_bsdf_eval(c, dot_wi_n, dot_wo_n);
-    vec3 reflection = microfacet_ggx_smith_eval(c.specular_f0, c.specular_f90, c.alpha, wi_, wo_, wh_);
-    vec3 reflectionMS = microfacet_ggx_smith_eval_ms(c.specular_f0, c.specular_f90, c.alpha, dot_wi_n, dot_wo_n);
+    vec3 diffuse = diffuse_bsdf_eval(c, wi, wo, g);
+    vec3 reflection = microfacet_ggx_smith_eval(c.specular_f0, c.specular_f90, c.alpha, wi, wo, wh, g);
+    vec3 reflectionMS = microfacet_ggx_smith_eval_ms(c.specular_f0, c.specular_f90, c.alpha, wi, wo, g);
     vec3 base = diffuse + reflection + reflectionMS;
 
-    vec3 sheen_base = sheen_layer(base, c.sheen, c.sheen_color, c.sheen_roughness, dot_wi_n, dot_wo_n, dot_wh_n);
-    vec3 combined = coating_layer(sheen_base, c.clearcoat, c.clearcoat_roughness, wi_, wo_, wh_);
+    vec3 sheen_base = sheen_layer(base, c.sheen, c.sheen_color, c.sheen_roughness, wi, wo, wh, g);
+    vec3 combined = coating_layer(sheen_base, c.clearcoat, c.clearcoat_roughness, wi, wo, wh, g);
 
     return combined;
 }
@@ -330,6 +335,11 @@ float luminance(vec3 rgb) {
 }
 
 vec3 dspbr_sample(const in MaterialClosure c, vec3 wi, in vec3 uvw, out vec3 weight, out float pdf) {
+    Geometry g;
+    g.n = c.n;
+    g.t = c.t;
+    g.b = cross(c.n, c.t);
+
     vec3 wi_ = c.to_local * wi;
     float dot_wi_n = wi_.z;
 
