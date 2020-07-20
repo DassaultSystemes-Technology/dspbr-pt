3ae2665855e175c99a97dfad3f3c76beb760817a
Bastian Sdorra <bsdorra@gmail.com>
2020-07-20 11:43:08 +0200

    Reverted wrong normal fix from 217ec96

--- lib/shader/utils.glsl
+++ lib/shader/utils.glsl
@@ -44,7 +44,7 @@ vec3 clamp_normal(vec3 n, vec3 ng, vec3 wi)
 // bottom (two-sided materials).
 void fix_normals(inout vec3 n, inout vec3 ng, in vec3 wi) {
     ng = dot(wi, ng) < 0.0 ? -ng : ng;
-    n = dot(wi, n) < 0.0 ? -n : n;
+    n = dot(ng, n) < 0.0 ? -n : n;
 }
 
 vec3 fix_normal(in vec3 n, in vec3 wi) {
