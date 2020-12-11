8102b6e2f0a54d3b213843a9da0ab144a44dc226
SDORRA Bastian <Bastian.SDORRA@3ds.com>
2020-12-11 19:45:44 +0100

    Ported app and renderer to ts

---
+++ tsconfig.js
@@ -0,0 +1,17 @@
+{
+  "compilerOptions": {
+    "target": "es6",
+    "moduleResolution": "node",
+    "sourceMap": true,
+    "removeComments": true,
+    "noImplicitAny": true,
+    "strict": true,
+    "noUnusedParameters": true,
+    "noImplicitReturns": true,
+    "noFallthroughCasesInSwitch": true
+  },
+  "include": [
+    "./src/**/*.ts",
+    "./lib/**/*.ts"
+  ]
+}
