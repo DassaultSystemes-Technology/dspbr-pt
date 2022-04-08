558a58e3daa2ec04a612333078fbdf32db5cd479
SDORRA Bastian <Bastian.SDORRA@3ds.com>
2022-04-08 14:07:02 +0200

    Update asset index, demo dir

--- src/viewer/asset_index.ts
+++ src/viewer/asset_index.ts
@@ -8,11 +8,11 @@ export const scenes = {
 export const ibls = {
   "None": {},
   "Artist Workshop": {
-    "url": "https://raw.githubusercontent.com/DassaultSystemes-Technology/dspbr-pt/master/src/viewer/assets/env/artist_workshop_1k.hdr",
+    "url": "https://raw.githubusercontent.com/DassaultSystemes-Technology/dspbr-pt/demo/demo/assets/env/artist_workshop_1k.hdr",
     "credit": "by Oliksiy Yakovlyev from <a href=\"hdrihaven.com\">hdrihaven.com</a> <a href=\"https://creativecommons.org/publicdomain/zero/1.0/\">(CC0)</a>"
   },
   "Footprint Court": {
-    "url": "https://raw.githubusercontent.com/DassaultSystemes-Technology/dspbr-pt/master/src/viewer/assets/env/Footprint_Court_Env.hdr",
+    "url": "https://raw.githubusercontent.com/DassaultSystemes-Technology/dspbr-pt/demo/demo/assets/env/Footprint_Court_Env.hdr",
     "credit": "from <a href=\"http://www.hdrlabs.com/sibl/archive.html\">hdrlabs.com</a> <a href=\"https://creativecommons.org/licenses/by-nc-sa/3.0/\">(CC BY-NC-SA 3.0)</a>",
     "intensity": 1.5
   }
